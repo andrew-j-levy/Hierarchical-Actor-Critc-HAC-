8f3c5290a493ff43437e78f453a1460c266d2582
Andrew Levy <andrew_levy2@brown.edu>
2018-10-13 17:58:54 -0400

    See README for updates

--- initialize_HAC.py
+++ initialize_HAC.py
@@ -1,5 +1,6 @@
 """
-This is the starting file for the Hierarchical Actor-Critc (HAC) algorithm.  The below script processes the command-line options specified by the user and instantiates the environment and agent. 
+This is the starting file for the Hierarchical Actor-Critc (HAC) algorithm.  The below script processes the command-line options specified
+by the user and instantiates the environment and agent. 
 """
 
 from design_agent_and_env import design_agent_and_env
@@ -10,7 +11,7 @@ from run_HAC import run_HAC
 # Determine training options specified by user.  The full list of available options can be found in "options.py" file.
 FLAGS = parse_options()
 
-# Instantiate the Mujoco environment.  The designer must provide the information requested in the "design_env.py" file. 
+# Instantiate the agent and Mujoco environment.  The designer must assign values to the hyperparameters listed in the "design_agent_and_env.py" file. 
 agent, env = design_agent_and_env(FLAGS)
 
 # Begin training
